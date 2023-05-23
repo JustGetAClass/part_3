a7022a2eac0bade4b68a6c9b510a3a82985128a8
JustGetAClass <mohamedsalat62@gmail.com>
2023-05-23 13:06:22 +0300

    added error handling for name and number

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
@@ -79,14 +81,6 @@ app.delete("/api/persons/:id", (req, res, next) => {
 
 app.post("/api/persons", (req, res, next) => {
   const body = req.body;
-
-  if (body.name === undefined) {
-    return res.status(400).json({ error: "name missing" });
-  }
-
-  if (body.number === undefined) {
-    return res.status(400).json({ error: "number missing" });
-  }
 
   const person = new Person({
     name: body.name,
